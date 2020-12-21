5aa14dd31ea80f2f0ad189d2b027d163d41b5d01
YelenaLadygin <75090782+YelenaLadygin@users.noreply.github.com>
2020-12-21 19:49:52 +0200

    Create Targil.java

---
+++ Targil.java
@@ -0,0 +1,92 @@
+// Task 4 : 
+    int x = 5;
+    int y = (12+20);
+    float z = 7.5f;
+ 
+// Task 5 :
+   int a = 5;
+   int b = 7;
+   int sum = (a+b);
+   int multiply = (a * b);
+   
+  //Task 6:
+     String color = "green";
+     System.out.println (color);
+     
+  //Task 7:
+      Scanner s = new Scanner(System.in);
+      System.out.println(" Enter your name :");
+      String name = s.nextLine();
+      System.out.println("Hello "+  name +","+" Welcome to Java!");
+      
+  //Task 8: 
+      int year = 2020;
+      year = (2020+10-30);
+      System.out.print(year);
+       
+ //Task 9:
+       Scanner s = new Scanner(System.in);
+       System.out.println(" Enter 3 int numbers :");
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int c = s.nextInt();
+        System.out.println( "Average =  "+ (a+b+c)/3);
+        System.out.println( "Goodbye... ");
+        
+// Task 10 :
+  Scanner s = new Scanner(System.in);
+  System.out.println(" Enter speed :");
+   int speed = s.nextInt();
+      if ( speed > 120 )
+         System.out.println(" too fast");
+     else if (speed < 80)
+         System.out.println(" too slow");
+     else
+        System.out.println(" good driver");
+        System.out.println( "Goodbye... ");
+         
+   // Task11: 
+     Scanner s = new Scanner(System.in);
+     System.out.println(" Enter Tomer`s and Danny`s ages :");
+     int tomerAge = s.nextInt();
+     int dannyAge = s.nextInt();
+       if (tomerAge > dannyAge )
+         System.out.println("Tomer is older");
+       else 
+          System.out.println("Danny is older");
+        System.out.println( "Goodbye... ");
+        
+ //Task 12:      
+       Scanner s = new Scanner(System.in);
+       System.out.println(" Enter 2 int numbers :");
+        int a = s.nextInt();
+        int b = s.nextInt();
+        if ( a == b )
+          System.out.println("equal");
+        else if ( a < b)
+            System.out.println(a);
+        else
+            System.out.println(b);
+        System.out.println( "Goodbye... ");
+        
+  //Task 15: 
+        int x ;
+        for ( x= 0; x<=10 ; x++){
+            System.out.println(x);
+        }
+        System.out.println( "Goodbye... ");
+        
+  //Task 16: 
+        int x ;
+        for ( x= 1; x<=50 ; x+=2){
+            System.out.println(x);
+        }
+        System.out.println( "Goodbye... ");
+        
+   
+   
+        
+   
+         
+ 
+     
